fb1f155fa432978c0f4074713cfd68b2d49be87b
Ida Rosvall <ir222gn@student.lnu.se>
2021-04-06 19:17:12 +0200

    Setup to test recieve messages from the api gateway

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ApiGatewayService } from '../services/api-gateway.service'
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+  message: String = '';
+
+  constructor(private apiGatewayService: ApiGatewayService) { }
+
+  ngOnInit(): void {
+    this.apiGatewayService.getWelcomeText()
+      .subscribe(object => this.message = object.message); 
+  }
+}
