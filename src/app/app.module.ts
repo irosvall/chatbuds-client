40c8171bdd7d0776c5d70f69406ddfa1674ef925
Ida Rosvall <ir222gn@student.lnu.se>
2021-04-08 09:38:00 +0200

    Add validation to the register form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { RegisterComponent } from './register/register.component';
+
+import { PasswordEqualValidatorDirective } from './validators/password-equal.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     RegisterComponent,
+    PasswordEqualValidatorDirective,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,23 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { passwordEqualValidator } from '../validators/password-equal.directive';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
-  registerForm = new FormGroup({
-    username: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
-    passwordRepeat: new FormControl('', Validators.required),
-  })
+export class RegisterComponent implements OnInit {
+  registerForm : FormGroup
   
   constructor() { }
 
+  ngOnInit(): void {
+    this.initForm()
+  }
+
   onSubmit() {
     // TODO: Use EventEmitter with form value
-    console.warn(this.registerForm.value);
+    console.warn(this.registerForm.value)
+  }
+
+  /**
+   * Intializes the registration form.
+   */
+  private initForm() : void {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      passwordRepeat: new FormControl('', Validators.required),
+    }, { validators: passwordEqualValidator })
+  }
+
+  get username() {
+    return this.registerForm.get('username')
+  }
+
+  get email() {
+    return this.registerForm.get('email')
+  }
+
+  get password() {
+    return this.registerForm.get('password')
+  }
+
+  get passwordRepeat() {
+    return this.registerForm.get('passwordRepeat')
   }
 }
