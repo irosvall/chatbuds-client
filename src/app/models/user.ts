fc9a027eac412d697788d5ea3422923715a0a470
Ida Rosvall <ir222gn@student.lnu.se>
2021-05-10 14:16:13 +0200

    Update the user model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,12 +1,11 @@
 import { Chat } from "./chat";
 
 export interface User {
-  username: string;
   userID: string;
+  username?: string;
   email?: string;
   about?: string;
   friends?: Array<User>;
-  chats?: Array<Chat>;
   createdAt?: string;
   updatedAt?: string;
 }
